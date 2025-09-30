a60180690eb0fb71d6eb4dc3382512138eb30c75
Mohan Gupta <mohangupta@live.com>
2025-09-30 22:28:35 +1000

    led works

--- hello-led/src/bin/main.rs
+++ hello-led/src/bin/main.rs
@@ -12,6 +12,12 @@ use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::systimer::SystemTimer;
 use log::info;
+use esp_hal::{rmt::Rmt, time::Rate};
+use esp_hal_smartled::{SmartLedsAdapterAsync, buffer_size_async};
+use smart_leds::{
+    RGB8, SmartLedsWriteAsync, brightness, gamma,
+    hsv::{Hsv, hsv2rgb},
+};
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
@@ -29,6 +35,30 @@ async fn main(spawner: Spawner) {
     let timer0 = SystemTimer::new(peripherals.SYSTIMER);
     esp_hal_embassy::init(timer0.alarm0);
 
+        // Configure RMT (Remote Control Transceiver) peripheral globally
+    // <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/rmt.html>
+    let rmt: Rmt<'_, esp_hal::Async> = {
+        let frequency: Rate =  Rate::from_mhz(80);
+        Rmt::new(peripherals.RMT, frequency)
+    }
+    .expect("Failed to initialize RMT")
+    .into_async();
+
+    // We use one of the RMT channels to instantiate a `SmartLedsAdapterAsync` which can
+    // be used directly with all `smart_led` implementations
+    let rmt_channel = rmt.channel0;
+    let rmt_buffer = [0_u32; buffer_size_async(1)];
+
+    let mut led: SmartLedsAdapterAsync<_, 25> =  SmartLedsAdapterAsync::new(rmt_channel, peripherals.GPIO2, rmt_buffer);
+
+    let mut color = Hsv {
+        hue: 0,
+        sat: 255,
+        val: 255,
+    };
+    let mut data: RGB8;
+    let level = 10;
+
     info!("Embassy initialized!\r");
 
     // TODO: Spawn some tasks
@@ -36,7 +66,19 @@ async fn main(spawner: Spawner) {
 
     loop {
         info!("Hello world!\r");
-        Timer::after(Duration::from_secs(1)).await;
+        for hue in 0..=255 {
+            color.hue = hue;
+            // Convert from the HSV color space (where we can easily transition from one
+            // color to the other) to the RGB color space that we can then send to the LED
+            data = hsv2rgb(color);
+            // When sending to the LED, we do a gamma correction first (see smart_leds
+            // documentation for details) and then limit the brightness to 10 out of 255 so
+            // that the output is not too bright.
+            led.write(brightness(gamma([data].into_iter()), level))
+                .await
+                .unwrap();
+            Timer::after(Duration::from_millis(100)).await;
+        }
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
