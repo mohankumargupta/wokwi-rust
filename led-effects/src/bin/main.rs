8ad2bd168d3f9b8bc8f234d35f514c4b610bcd5c
Mohan Gupta <mohangupta@live.com>
2025-10-01 16:01:58 +1000

    blocking version of SmartLedsAdapter

--- led-effects/src/bin/main.rs
+++ led-effects/src/bin/main.rs
@@ -6,38 +6,33 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
+// --- IMPORTS CHANGED ---
 use esp_backtrace as _;
-use esp_hal::{clock::CpuClock, time::Instant};
-use esp_hal::timer::systimer::SystemTimer;
-use esp_println::println;
-use esp_hal::{rmt::Rmt, time::Rate};
-use esp_hal_smartled::{SmartLedsAdapterAsync, buffer_size_async};
-//use log::info;
-use smart_leds::{
-    RGB8, SmartLedsWriteAsync, brightness, gamma,
-    hsv::{Hsv, hsv2rgb}
+use esp_hal::{
+    clock::CpuClock,
+    delay::Delay,
+    main,
+    rmt::Rmt,
+    time::{Instant, Rate},
 };
+use esp_println::println;
+use esp_hal_smartled::{SmartLedsAdapter, smart_led_buffer};
 use led_effects::controller::EffectController;
 use led_effects::solid_effect::SolidColor;
-
-
+use smart_leds::{RGB8, SmartLedsWrite};
 
 extern crate alloc;
 use alloc::boxed::Box;
-use alloc::vec::Vec;
+// use alloc::vec::Vec; // No longer needed
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
-const NUM_LEDS: usize = 4;
+const NUM_LEDS: usize = 12;
 
-#[esp_hal_embassy::main]
-async fn main(spawner: Spawner) {
-    // generator version: 0.5.0
-
+// --- MAIN FUNCTION CHANGED TO BLOCKING ---
+#[main]
+fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -45,60 +40,51 @@ async fn main(spawner: Spawner) {
 
     esp_alloc::heap_allocator!(size: 64 * 1024);
 
-    let timer0 = SystemTimer::new(peripherals.SYSTIMER);
-    esp_hal_embassy::init(timer0.alarm0);
-
     println!("Setup done.\r");
 
-
-        // Configure RMT (Remote Control Transceiver) peripheral globally
-    // <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/peripherals/rmt.html>
-    let rmt: Rmt<'_, esp_hal::Async> = {
-        let frequency: Rate =  Rate::from_mhz(80);
+    // --- RMT INITIALIZATION CHANGED TO BLOCKING ---
+    let rmt: Rmt<'_, esp_hal::Blocking> = {
+        let frequency: Rate = Rate::from_mhz(80);
         Rmt::new(peripherals.RMT, frequency)
     }
-    .expect("Failed to initialize RMT")
-    .into_async();
+    .expect("Failed to initialize RMT");
 
-    // We use one of the RMT channels to instantiate a `SmartLedsAdapterAsync` which can
-    // be used directly with all `smart_led` implementations
     let rmt_channel = rmt.channel0;
-    let rmt_buffer = [0_u32; buffer_size_async(4)];
 
-    let mut led: SmartLedsAdapterAsync<_, 100> =  SmartLedsAdapterAsync::new(rmt_channel, peripherals.GPIO3, rmt_buffer);
-
-
+    // --- BUFFER AND LED ADAPTER CHANGED TO BLOCKING VERSIONS ---
+    let rmt_buffer = smart_led_buffer!(NUM_LEDS);
+    let mut led = SmartLedsAdapter::new(rmt_channel, peripherals.GPIO3, rmt_buffer);
+    
     println!("LED Setup done.\r");
 
     let mut leds: [RGB8; NUM_LEDS] = [RGB8::default(); NUM_LEDS];
     let mut last_update = Instant::now();
 
-        // -- Create and populate the EffectController --
     let mut effect_controller = EffectController::new();
-    effect_controller.add_effect(Box::new(SolidColor { color: RGB8::new(255, 0, 0) }));
+    effect_controller.add_effect(Box::new(SolidColor {
+        color: RGB8::new(255, 0, 0),
+    }));
+    
+    let delay = Delay::new();
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
-
+    // --- LOOP CHANGED TO USE BLOCKING CALLS ---
     loop {
         let now = Instant::now();
-        let delta: f32 = 0.0;
+       let delta = (now - last_update).as_millis() as f32;
         last_update = now;
 
         let current_effect = effect_controller.get_current_effect();
         current_effect.before_render(delta);
 
-        // -- render --
         for i in 0..NUM_LEDS {
             leds[i] = current_effect.render(i, NUM_LEDS);
         }
 
+        // --- WRITE CALL IS NOW BLOCKING (NO .await) ---
+        led.write(leds.iter().cloned()).unwrap();
 
-        led.write(brightness(leds.iter().cloned(), 255)).await.unwrap();
-        
-        
-        Timer::after(Duration::from_secs(1)).await;
+        // --- DELAY IS NOW BLOCKING ---
+        //delay.delay_ms(1000u32);
+        delay.delay_millis(1000u32);
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
 }
