e07993b3f28c9b80fa60bf5600b95397ef9b524a
Mohan Gupta <mohangupta@live.com>
2025-10-02 15:32:56 +1000

    epaper: compiles

--- epaper/src/bin/main.rs
+++ epaper/src/bin/main.rs
@@ -19,17 +19,21 @@ use esp_hal::{
         master::{Config, Spi},
     },
     time::Rate,
-
 };
 use log::info;
 use epd_waveshare::{epd2in9_v2::*, graphics::DisplayRotation, prelude::*};
+use embedded_hal_bus::spi::ExclusiveDevice;
 
 extern crate alloc;
 
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
+    // generator version: 0.5.0
+
     esp_println::logger::init_logger_from_env();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -40,7 +44,9 @@ async fn main(spawner: Spawner) {
     let timer0 = TimerGroup::new(peripherals.TIMG1);
     esp_hal_embassy::init(timer0.timer0);
 
-    // --- SPI setup (blocking) ---
+    info!("Embassy initialized!");
+
+    //SPI (blocking)
     let sclk = peripherals.GPIO18;
     let mosi = peripherals.GPIO23;
     let cs = Output::new(peripherals.GPIO5, Level::Low, OutputConfig::default());//peripherals.GPIO5;
@@ -53,24 +59,31 @@ async fn main(spawner: Spawner) {
     )
     .unwrap()
     .with_sck(sclk)
-    .with_mosi(mosi);
-    // note: no `.into_async()`, no `.with_cs()`
+    .with_mosi(mosi);    
 
-    info!("Embassy initialized!");
-
-    // --- EPD pins ---
+    //epaper setup
     let busy = Input::new(peripherals.GPIO4, InputConfig::default());//peripherals.GPIO4;
     let reset = Output::new(peripherals.GPIO21, Level::Low, OutputConfig::default());//peripherals.GPIO21;
     let dc = Output::new(peripherals.GPIO22, Level::Low, OutputConfig::default());//peripherals.GPIO22;
 
-    let mut delay = Delay::default();
+    let mut delay = Delay::new();
+    let mut spi_dev = ExclusiveDevice::new(spi, cs, delay).expect("Failed to create SPI device");
 
-    let mut epd = Epd2in9::new(&mut spi, busy,cs,  dc, reset, &mut delay)
+    let mut epd = Epd2in9::new(&mut spi_dev, busy, dc, reset, &mut delay, None)
        .expect("EPD init failed");
 
-    // --- Loop ---
+
+
+
+    // main loop
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
     loop {
         info!("Hello world!");
         Timer::after(Duration::from_secs(1)).await;
     }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
 }
