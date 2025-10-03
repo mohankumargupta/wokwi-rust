7d05dac94c5b84fcd763645a3bf2cbf767c35038
Mohan Gupta <mohangupta@live.com>
2025-10-04 09:30:23 +1000

    got firmware

---
+++ led-effects/copy.py
@@ -0,0 +1,16 @@
+import sys
+import shutil
+from pathlib import Path
+
+def main():
+    build_base = Path("./target") / "riscv32imc-unknown-none-elf" / "debug" 
+    firmware_bin = "led-effects"
+    firmware_bin_full_path = build_base / firmware_bin 
+
+    dest_dir = Path("./firmware")
+    dest_dir.mkdir(exist_ok=True)
+    print(firmware_bin_full_path)
+    shutil.copy2(firmware_bin_full_path, dest_dir / firmware_bin)
+
+if __name__ == "__main__":
+    main()
